c93d07108f26bd72c28ff7f3810df561b1f22107
Bhavya Suthar <bhavyasuthar06@gmail.com>
2024-08-04 19:06:05 +0530

    logout

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,29 +1,36 @@
-import { Link } from "react-router-dom"
-import logo from '../assets/logo.svg'
+import { Link, NavLink } from "react-router-dom";
+import logo from "../assets/logo.svg";
 import logout from "../assets/logout.svg";
-import Navbar from "./Navbar"
-import { useState } from "react"
+import Navbar from "./Navbar";
+import { useState } from "react";
 import { MdClose, MdMenu } from "react-icons/md";
 import { FaOpencart } from "react-icons/fa";
 
 const Header = () => {
-
-  const [menuOpened , setMenuOpened] = useState(true)
+  const [menuOpened, setMenuOpened] = useState(false);
   const toggleMenu = () => setMenuOpened(!menuOpened);
-
 
   return (
     <header className="fixed top-0 left-0 m-auto max_padd_container w-full bg-white ring-1 ring-slate-900/5 z-10 ">
-    <div className="px-4 flexBetween py-3 max-xs:px-2">
+      <div className="px-4 flexBetween py-3 max-xs:px-2">
         <div>
-            <Link><img src={logo} alt="" height={66} width={88}/></Link>
+          <Link>
+            <img src={logo} alt="" height={66} width={88} />
+          </Link>
         </div>
         {/* Navbar Desktop*/}
-        <Navbar containerStyles={"hidden md:flex gap-x-5 xl:gap-x-10 medium-15"}/>
+        <Navbar
+          containerStyles={"hidden md:flex gap-x-5 xl:gap-x-10 medium-15"}
+        />
 
         {/* Navbar Mobile */}
-        <Navbar containerStyles={`${menuOpened ? "flex item-start flex-col-gap-y-12 fixed top-20 right-8 p-12 bg-white rounded-3xl shadow-md w-64 medium-16 ring-1 ring-slate-900/5 transition-all duration-300" 
-        : "flex item-start flex-col-gap-y-12 fixed top-20 right-8 p-12 bg-white rounded-3xl shadow-md w-64 medium-16 ring-1 ring-slate-900/5 transition-all duration-300"}`}/>
+        <Navbar
+          containerStyles={`${
+            menuOpened
+              ? "flex item-start flex-col gap-y-6 fixed top-20 right-5 p-12 bg-white rounded-3xl shadow-md w-64 medium-16 ring-1 ring-slate-900/5 transition-all duration-300"
+              : "flex item-start flex-col gap-y-12 fixed top-20 p-12 bg-white rounded-3xl shadow-md w-64 medium-16 ring-1 ring-slate-900/5 transition-all duration-300 -right-[100%]"
+          }`}
+        />
 
         {/* Buttons */}
         <div className="flexBetween sm:gap-x-6 bold-16">
@@ -46,7 +53,7 @@ const Header = () => {
         </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
