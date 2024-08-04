f618013566161ef443a451883857a6813f0036db
Bhavya Suthar <bhavyasuthar06@gmail.com>
2024-08-04 19:16:16 +0530

    login

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,17 +1,19 @@
 import { Link, NavLink } from "react-router-dom";
-import logo from "../assets/logo.svg";
-import logout from "../assets/logout.svg";
 import Navbar from "./Navbar";
 import { useState } from "react";
 import { MdClose, MdMenu } from "react-icons/md";
 import { FaOpencart } from "react-icons/fa";
+//import images
+import logo from "../assets/logo.svg";
+import logout from "../assets/logout.svg";
+import user from "../assets/user.svg";
 
 const Header = () => {
   const [menuOpened, setMenuOpened] = useState(false);
   const toggleMenu = () => setMenuOpened(!menuOpened);
 
   return (
-    <header className="fixed top-0 left-0 m-auto max_padd_container w-full bg-white ring-1 ring-slate-900/5 z-10 ">
+    <header className="fixed top-0 left-0 m-auto max_padd_container w-full bg-transparent ring-1 ring-slate-900/5 z-10 ">
       <div className="px-4 flexBetween py-3 max-xs:px-2">
         <div>
           <Link>
@@ -33,7 +35,7 @@ const Header = () => {
         />
 
         {/* Buttons */}
-        <div className="flexBetween sm:gap-x-6 bold-16">
+        <div className="flexBetween sm:gap-x-2 bold-16">
           {!menuOpened ? (
             <MdMenu
               className="md:hidden cursor-pointer hover:text-secondary mr-2 p-1 ring-1 ring-slate-900/30 h-8 w-8 rounded-full"
@@ -48,7 +50,9 @@ const Header = () => {
           <div className="flexBetween cursor-pointer sm:gap-x-6">
             <NavLink to={"cart-page"} className={"flex"}><FaOpencart className="p-1 h-8 w-8 ring-slate-900/30 ring-1 rounded-full"/>
             <span className="relative flexCenter w-5 h-5 rounded-full bg-secondary text-white medium-14 -top-2">0</span></NavLink>
-            <NavLink to={'logout'} className={"btn_secondary_rounded flexCenter"}><img src={logout}/>Logout</NavLink>
+            {/* <NavLink to={'logout'} className={"btn_secondary_rounded flexCenter gap-x-2 medium-16"}><img src={logout} alt="logoutIcon" height={19} width={19}/>Logout</NavLink> */}
+            <NavLink to={'login'} className={"btn_secondary_rounded flexCenter gap-x-2 medium-16"}><img src={user} alt="logoutIcon" height={19} width={19}/>Login</NavLink>
+
           </div>
         </div>
       </div>
