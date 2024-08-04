4ca6c5e97ada5aa098ea8514ebe77daa09b6080c
Bhavya Suthar <bhavyasuthar06@gmail.com>
2024-08-04 19:03:11 +0530

    responsive navbar

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom"
 import logo from '../assets/logo.svg'
+import logout from "../assets/logout.svg";
 import Navbar from "./Navbar"
 import { useState } from "react"
+import { MdClose, MdMenu } from "react-icons/md";
+import { FaOpencart } from "react-icons/fa";
 
 const Header = () => {
 
   const [menuOpened , setMenuOpened] = useState(true)
+  const toggleMenu = () => setMenuOpened(!menuOpened);
+
 
   return (
     <header className="fixed top-0 left-0 m-auto max_padd_container w-full bg-white ring-1 ring-slate-900/5 z-10 ">
@@ -21,8 +26,25 @@ const Header = () => {
         : "flex item-start flex-col-gap-y-12 fixed top-20 right-8 p-12 bg-white rounded-3xl shadow-md w-64 medium-16 ring-1 ring-slate-900/5 transition-all duration-300"}`}/>
 
         {/* Buttons */}
-        <div>Buttons</div>
-    </div>
+        <div className="flexBetween sm:gap-x-6 bold-16">
+          {!menuOpened ? (
+            <MdMenu
+              className="md:hidden cursor-pointer hover:text-secondary mr-2 p-1 ring-1 ring-slate-900/30 h-8 w-8 rounded-full"
+              onClick={toggleMenu}
+            />
+          ) : (
+            <MdClose
+              className="md:hidden cursor-pointer hover:text-secondary mr-2 p-1 ring-1 ring-slate-900/30 h-8 w-8 rounded-full"
+              onClick={toggleMenu}
+            />
+          )}
+          <div className="flexBetween cursor-pointer sm:gap-x-6">
+            <NavLink to={"cart-page"} className={"flex"}><FaOpencart className="p-1 h-8 w-8 ring-slate-900/30 ring-1 rounded-full"/>
+            <span className="relative flexCenter w-5 h-5 rounded-full bg-secondary text-white medium-14 -top-2">0</span></NavLink>
+            <NavLink to={'logout'} className={"btn_secondary_rounded flexCenter"}><img src={logout}/>Logout</NavLink>
+          </div>
+        </div>
+      </div>
     </header>
   )
 }
