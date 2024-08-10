6b0f8e8310941c332d3f0a01320bbf6f7cffddcb
Bhavya Suthar <bhavyasuthar06@gmail.com>
2024-08-10 20:00:04 +0530

    Popular Product

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import Hero from '../Components/Hero'
+import React from "react";
+import Hero from "../Components/Hero";
+import Popular from "../Components/Popular";
 
 const Home = () => {
   return (
     <>
-      <Hero/>
+      <Hero />
+      <Popular />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
