baa51342f8b9140aef8dee946a0ed875cd516ec5
Bhavya Suthar <bhavyasuthar06@gmail.com>
2024-08-21 22:30:45 +0530

    Modify Home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Hero from "../Components/Hero";
+import NewCollection from "../Components/NewCollection";
+import Offer from '../Components/Offer'
 import Popular from "../Components/Popular";
+import NewsLetter from "../Components/NewsLetter";
 
 const Home = () => {
   return (
     <>
       <Hero />
-      <Popular />
+      <Popular/>
+      <Offer/>
+      <NewCollection/>
+      <NewsLetter/>
     </>
   );
 };
