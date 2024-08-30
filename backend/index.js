f043360949a49486fbe53c58bfebd803573a934d
Bhavya Suthar <bhavyasuthar06@gmail.com>
2024-08-30 20:02:08 +0530

    modify backend

--- backend/index.js
+++ backend/index.js
@@ -16,9 +16,76 @@ mongoose.connect(
 );
 
 //API creation
-app.get("/",(req,res)=>{
-    res.send("Express App is running")
+app.get("/", (req, res) => {
+  res.send("Express App is running");
+});
+
+//Image storage engine
+const storage = multer.diskStorage({
+  destination: "./upload/images",
+  filename: (req, file, cb) => {
+    return cb(
+      null,
+      `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+
+const upload = multer({ storage: storage });
+//creating upload endpoint for images
+app.use("/images", express.static("upload/images"));
+app.post("/upload", upload.single("product"), (req, res) => {
+  res.json({
+    success: 1,
+    image_url: `http://localhost:${port}/images/${req.file.filename}`,
+  });
+});
+
+//schema for creating product
+const Product = mongoose.model("Product", {
+  id: {
+    type: Number,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  new_price: {
+    type: Number,
+    required: true,
+  },
+  old_price: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  available: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+app.post('/addproduct',async(req,res)=>{
+    const product = new Product({
+        id:req.body.id,
+        name:req.body.name,
+        image:req.body.image,
+        category:req.body.category
+    })
 })
+
 app.listen(port, (error) => {
   if (!error) {
     console.log("Server is running on port " + port);
