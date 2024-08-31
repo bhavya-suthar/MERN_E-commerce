8f62df2ce02afe07eb3637d942202707dc54fdeb
Bhavya Suthar <bhavyasuthar06@gmail.com>
2024-08-31 19:09:12 +0530

    add admin

--- backend/index.js
+++ backend/index.js
@@ -77,13 +77,51 @@ const Product = mongoose.model("Product", {
   },
 });
 
-app.post('/addproduct',async(req,res)=>{
-    const product = new Product({
-        id:req.body.id,
-        name:req.body.name,
-        image:req.body.image,
-        category:req.body.category
-    })
+//creating API for add product
+
+app.post("/addproduct", async (req, res) => {
+  let products = await Product.find({});
+  let id;
+  if (products.length > 0) {
+    let last_product_array = products.slice(-1);
+    let last_product = last_product_array[0];
+    id = last_product.id + 1;
+  }else{
+    id=1
+  }
+  const product = new Product({
+    // id: req.body.id,
+    id: id,
+    name: req.body.name,
+    image: req.body.image,
+    category: req.body.category,
+    new_price: req.body.new_price,
+    old_price: req.body.old_price,
+  });
+  console.log("🚀 ~ app.post ~ product:", product);
+  await product.save();
+  console.log("🚀 ~ app.post ~ save:");
+  res.json({
+    success: true,
+    name: req.body.name,
+  });
+});
+
+//creating API for remove product
+app.post('/removeproduct',async(req,res)=>{
+  await Product.findOneAndDelete({id:req.body.id});
+  console.log('removed')
+  res.json({
+    success:true,
+    name:req.body.name,
+  })
+})
+
+//creating API for get all products
+app.get('/allproducts',async(req,res)=>{
+  let products = await Product.find({});
+  console.log("All Products Fetched");
+  res.send(products)
 })
 
 app.listen(port, (error) => {
