a604721b118e4555238553297da677172c5e08d3
Bhavya Suthar <bhavyasuthar06@gmail.com>
2024-09-07 22:46:38 +0530

    modification

--- backend/index.js
+++ backend/index.js
@@ -179,6 +179,25 @@ app.post("/signup", async (req, res) => {
 });
 
 //creating endpoint for user login
+app.post('/login',async(req,res)=>{
+  let user = await User.findOne({email:req.body.email});
+  if(user){
+    const passMatch = req.body.password === user.password;
+    if(passMatch){
+      const data = {
+        user:{
+          id:user.id
+        }
+      }
+      const token = jwt.sign(data,'secret_ecom');
+      res.json({success:true,token});
+    }else{
+      res.json({success:false,errors:"Wrong Password"})
+    }
+  }else{
+    res.json({success:false,errors:"Wrong Email Address"})
+  }
+})
 
 app.listen(port, (error) => {
   if (!error) {
--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,24 +1,82 @@
+import { useState } from "react";
 
 const Login = () => {
+  const [state, setState] = useState("Login");
+
+  const login = async ()=>{
+    console.log(" inside login function")
+  }
+
+  const signup = async () =>{
+    console.log("inside sign up function")
+  }
+
   return (
     <section className="max_padd_container flexCenter flex-col pt-32">
-    <div className="max-w-[555px] h-[600px] bg-white m-auto px-14 py-10 rounded-md">
-    <h3 className="h3">Sign Up</h3>
-    <div className="flex flex-col gap-4 mt-7">
-      <input type="text" placeholder="Your Name" className="h-14 w-full pl-5 bg-slate-900/5 outline-none rounded-xl" />
-      <input type="text" placeholder="Email Address" className="h-14 w-full pl-5 bg-slate-900/5 outline-none rounded-xl" />
-      <input type="text" placeholder="Password" className="h-14 w-full pl-5 bg-slate-900/5 outline-none rounded-xl" />
-    </div>
-    <button className="btn_dark_rounded my-5 w-full !rounded-md">Countinue</button>
-    <p className="text-black font-bold">Already have an account? 
-    <span className="text-secondary underline cursor-pointer">Login</span></p>
-    <div className="flexCenter mt-6 gap-3"> 
-      <input type="checkbox" name="" id="" />
-      <p>By continuing, i agree to the terms of use & privacy policy</p>
-    </div>
-    </div>
+      <div className="max-w-[555px] h-[600px] bg-white m-auto px-14 py-10 rounded-md">
+        <h3 className="h3">{state}</h3>
+        <div className="flex flex-col gap-4 mt-7">
+          {state === "Sign Up" ? (
+            <input
+              type="text"
+              name="username"
+              placeholder="Your Name"
+              className="h-14 w-full pl-5 bg-slate-900/5 outline-none rounded-xl"
+            />
+          ) : (
+            ""
+          )}
+          <input
+            type="text"
+            placeholder="Email Address"
+            className="h-14 w-full pl-5 bg-slate-900/5 outline-none rounded-xl"
+          />
+          <input
+            type="text"
+            placeholder="Password"
+            className="h-14 w-full pl-5 bg-slate-900/5 outline-none rounded-xl"
+          />
+        </div>
+        <button
+          onClick={() => {
+            state == "Login" ? login() : signup();
+          }}
+          className="btn_dark_rounded my-5 w-full !rounded-md"
+        >
+          Countinue
+        </button>
+        {state === "Sign Up" ? (
+          <p className="text-black font-bold">
+            Already have an account?
+            <span
+              onClick={() => {
+                setState("Login");
+              }}
+              className="text-secondary underline cursor-pointer"
+            >
+              Login
+            </span>
+          </p>
+        ) : (
+          <p className="text-black font-bold">
+            Create an account?
+            <span
+              onClick={() => {
+                setState("Sign Up");
+              }}
+              className="text-secondary underline cursor-pointer"
+            >
+              Click Here
+            </span>
+          </p>
+        )}
+        <div className="flexCenter mt-6 gap-3">
+          <input type="checkbox" name="" id="" />
+          <p>By continuing, i agree to the terms of use & privacy policy</p>
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
